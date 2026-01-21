6e8e134786f7b09bddc6d2202c8c3d97ea02865a
eluvvs <vohnouta24@spstrutnov.cz>
2026-01-21 10:17:54 +0100

    Visual Studio Solution creation

---
+++ WPF/WPF Try-out/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_Try_out
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
